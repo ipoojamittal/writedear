603593e88846ae3e78c3c592fd696386299b27e6
Pooja Mittal <ipoojamittal@icloud.com>
2025-07-30 06:57:20 +0530

    User Profile update

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -6,7 +6,12 @@ import { FaCheck } from "react-icons/fa";
 
 
 const UserProfile = () => {
-  const[avatar, setAvatar] = useState('')
+  const [avatar, setAvatar] = useState(Avatar)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [currentPassword, setCurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [confirmNewPassword, setConfirmNewPassword ] = useState('')
   return (
     <section className='profile'>
       <div className='container profile__container'>
@@ -14,7 +19,7 @@ const UserProfile = () => {
           <div className='profile__details'>
             <div className='avatar__wrapper'>
               <div className='profile__avatar'> 
-                <img src={Avatar} alt='' />
+                <img src={avatar} alt='' />
               </div>
               {/*Form to update avatar*/}
 
@@ -27,6 +32,21 @@ const UserProfile = () => {
 
             </div>
             <h1>Pooja Mittal</h1>
+
+            {/* form to update user details */}
+            <form className='form profile__form'>
+              <p className='form__error-message'>This is an error message</p>
+              <input type="text" placeholder='Full Name' value={name} onChange={e => setName(e.target.value)}/>
+              <input type="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/>
+              <input type="password" placeholder='Current Password' value={currentPassword} onChange={e => setCurrentPassword(e.target.value)}/>
+              <input type="password" placeholder='New Password' value={newPassword} onChange={e => setNewPassword(e.target.value)}/>
+              <input type="password" placeholder='Confirm New Password' value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)}/>
+           
+
+              <button type="submit" className='btn primary'>Update details</button>
+
+            
+            </form>
           </div>
       </div>
     </section>
